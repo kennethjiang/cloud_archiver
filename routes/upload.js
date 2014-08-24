3198423697a4f46c05b708caf99d088528f7a734
Kenneth Jiang <kenneth.jiang@gmail.com>
2014-08-24 02:23:43 +0000

    upload to s3

--- routes/upload.js
+++ routes/upload.js
@@ -3,8 +3,12 @@ var util = require('util');
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config/aws.json');
+
 router.get('/', function(req, res) {
-  res.render('upload_select');
+    res.render('upload_select');
 });
 
 router.post('/', function(req, res) {
@@ -12,7 +16,17 @@ router.post('/', function(req, res) {
     var form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files) {
-      res.send( 'received upload:' + util.inspect({fields: fields, files: files}));
+        if (err) throw err;
+
+        fs.readFile(files.filedata.path, function (err, data) {
+	    if (err) throw err;
+        
+            var s3 = new AWS.S3({params: {Bucket: 'cloud-archiver/1', Key: files.filedata.name}});
+	    s3.putObject({Body: data}, function(err, data) {
+	        if (err) throw err;
+                res.send( 'received upload:' + util.inspect({fields: fields, files: files}));
+	    });
+	});
     });
 });
 
